55c2a32d6b7e1ca015670f24fe04f3ff7d3fd775
Pontus Ånimmer <PontusDator@Pontuss-Air-2.lan>
2020-11-10 15:36:29 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import AppNavBar from "./components/AppNavBar";
+import ShoppingList from "./components/ShoppingList";
+import { Container } from "reactstrap";
+
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/authActions";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import ItemModal from "./components/ItemModal";
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <AppNavBar />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
+      </div>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const config = require("config");
+
+const app = express();
+
+app.use(express.json());
+
+const db = config.get("mongoURI");
+
+// const mongoURI =
+//   "mongodb+srv://Ponta1337:Hejhej123@cluster0.hmxny.mongodb.net/Items?retryWrites=true&w=majority";
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected…");
+  })
+  .catch((err) => console.log(err));
+
+app.use("/api/items", require("./routes/api/items"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+
+// serve static assets if in prodution
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on ${port}`));
